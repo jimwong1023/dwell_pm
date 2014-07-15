14f35263989326c9d3c806d9c64969ea1fc4e702
Jimmy <jimwong1023@gmail.com>
2014-07-15 12:55:58 -0700

    assignment tests

---
+++ spec/models/assignment_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Assignment do
+  it { should belong_to :project }
+  it { should belong_to :user }
+  it { should respond_to :user }
+  it { should respond_to :project }
+end
